657f6791020d2aeec69e4a2f008cda96b54b8a46
marlonyapwere <marloneenyapwere@gmail.com>
2024-02-19 10:39:33 +0200

    2.16 Create Api inside accounts microservice

---
+++ accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,10 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+    /**
+     * @param customerDto - CustomerDto Object
+     */
+    void createAccount(CustomerDto customerDto);
+}
